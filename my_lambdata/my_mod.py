289dcebc68eae5cddfee766eca241e6259c492bb
Becca <beccacauthorn@gmail.com>
2020-03-12 00:07:13 -0400

    import function from mod

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,5 +1,5 @@
 
  #my_lambdata/my_mod.py
 
- def enlarge(n):
+def enlarge(n):
     return n * 100
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,7 +1,13 @@
 
 import pandas
 
+from my_lambdata.my_mod import enlarge
+
+
 print("Happy Birthday")
 
 df = pandas.DataFrame({"x":[1,2,3], "y":[4,5,6]})
 print(df.head())
+
+x = 5
+print("ENLARGE", x, "TO", enlarge(x))
